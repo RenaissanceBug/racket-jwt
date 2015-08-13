65b930053fbf13a793fa3b6b7d4eac519fc5ba2d
Jordan Johnson <jmj@fellowhuman.com>
2015-08-13 08:27:19 -0700

    Fix deps.

--- info.rkt
+++ info.rkt
@@ -2,13 +2,18 @@
 
 (define collection 'multi)
 
-(define deps '("srfi-lite-lib" "base" "typed-racket-more" "grommet"))
+(define deps '("srfi-lite-lib"
+               "base"
+               "typed-racket-lib"
+               "typed-racket-more"
+               "unstable-contract-lib"
+               "grommet"))
 
 (define build-deps
   '("rackunit-lib" "web-server-lib" "racket-doc" "scribble-lib"
-    "typed-racket-more" "typed-racket-doc" ;; for typed/json
+    "typed-racket-lib" "typed-racket-more" "typed-racket-doc"
     "option-bind"
     ;"net-doc"
     ))
 
-(define version "1.0")
+(define version "1.0.1")
